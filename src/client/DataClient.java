b2fd3f408fffa031660be7c99499b821788db657
vbalan <vbalan@patriot.ambianta.local>
2020-12-18 13:19:14 +0200

    final with Jackson

--- src/client/DataClient.java
+++ src/client/DataClient.java
@@ -4,42 +4,81 @@ import java.net.InetAddress;
 import java.net.Socket;
 import server.DataServer;
 import java.util.List;
-import org.json.JSONObject;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.fasterxml.jackson.core.StreamReadFeature;
+import com.fasterxml.jackson.core.StreamWriteFeature;
+import java.io.OutputStream;
+import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.IOException;
 
-
-public class DataClient {
-
+public class DataClient{
+	
 	private List<Integer> data;
+	private String option;
+	
+	public DataClient() {		
+	}
 		
-	public DataClient(List<Integer> data) {
+	public DataClient(List<Integer> data, String option) {
+		this.data = data;
+		if(option.equals("positive") || option.contentEquals("negative"))
+			this.option  = option;
+	}	
+	
+	public List<Integer> getData() {
+		return data;
+	}
+
+	public void setData(List<Integer> data) {
 		this.data = data;
 	}
+
+	public String getOption() {
+		return option;
+	}
+
+	public void setOption(String option) {
+		this.option = option;
+	}
+
+	@Override
+	public String toString() {
+		return "DataClient [data=" + data + ", option=" + option + "]";
+	}
 	
-	public void getData() throws IOException, ClassNotFoundException{
+	public void getDataForTransfer() throws IOException {
+		
 		Socket clientSocket = new Socket(InetAddress.getLoopbackAddress(), DataServer.PORT);
-		ObjectOutputStream dout = new ObjectOutputStream(clientSocket.getOutputStream());
-		String out = "";
-		for(int i = 0; i < data.size(); i++) {
-			out = out + "\"Key"+ i + "\":" + "\"" + data.get(i) + "\"";
-			if(i != data.size() - 1)
-				out = out + ", ";			
-		}
-		JSONObject jsonObject = new JSONObject(out);
-		System.out.println(jsonObject);
-		dout.writeObject(jsonObject);
-		System.out.println("Client is transfered data");
-		dout.flush();
-		ObjectInputStream din = new ObjectInputStream(clientSocket.getInputStream());
-		System.out.println("This is responce from server: " + din.readObject());
+		
+		OutputStream dout = new ObjectOutputStream(clientSocket.getOutputStream());
+		
+		ObjectMapper objectMapper = JsonMapper.builder().disable(StreamWriteFeature.AUTO_CLOSE_TARGET)
+					  									.disable(StreamReadFeature.AUTO_CLOSE_SOURCE)
+					  									.enable(StreamWriteFeature.FLUSH_PASSED_TO_STREAM)
+					  									.build();
+		
+		objectMapper.writeValue(dout, this);
+		System.out.println("Client is transfering data: " + this.toString());
+		
+		InputStream din = new ObjectInputStream(clientSocket.getInputStream());
+		String average = objectMapper.readValue(din, String.class);
+		System.out.println("Сlient received data from the server: " + average);
+		
+		DataClient d = new DataClient(List.of(10, 20, -30, -40), "negative");
+		
+		objectMapper.writeValue(dout, d);
+		System.out.println("Client is transfering data: " + d.toString());
+		
+		String average_1 = objectMapper.readValue(din, String.class);
+		System.out.println("Сlient received data from the server: " + average_1);
+		
 		clientSocket.close();
 	}
 	
 	public static void main(String[] args) throws IOException, ClassNotFoundException{		
-		List<Integer> forTransmition = List.of(10, 20 , 30 , 40);
-		DataClient dataClient = new DataClient(forTransmition);
-		dataClient.getData();		
+		new DataClient(List.of(10, 20, 30, 41), "positive").getDataForTransfer();
 	}
 }
--- src/server/DataServer.java
+++ src/server/DataServer.java
@@ -2,51 +2,69 @@ package server;
 
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.ArrayList;
-import java.util.List;
-
-import org.json.JSONObject;
-
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.fasterxml.jackson.core.StreamReadFeature;
+import com.fasterxml.jackson.core.StreamWriteFeature;
+import client.DataClient;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.IOException;
 
 public class DataServer {
-	
+
 	public static final Integer PORT = 8888;
 	
-	public static Socket start() throws IOException {
+	public Socket startServer() throws IOException {
 		ServerSocket serverSocket = new ServerSocket(PORT);
 		Socket clientSocket = serverSocket.accept();
 		serverSocket.close();
 		return clientSocket;
 	}
 	
-	public static void main(String[] args) throws IOException, ClassNotFoundException {
-		Socket clientSocket = DataServer.start();
-		ObjectInputStream din = new ObjectInputStream(clientSocket.getInputStream());
-		JSONObject clientData = (JSONObject) din.readObject();
-		System.out.println("Server is reading transfered data: " + clientData);
-		List<Integer> clientDataFormated = new ArrayList<>();
-		String str = "";
-//		for(var s : clientData.toCharArray()) {
-//			if(s != ',' && s != ' ')
-//				str = str + s;
-//			else if(s == ','){
-//				clientDataFormated.add(Integer.parseInt(str));
-//				str = "";
-//			}
-//		}
-		Integer average = null;
-		for(int i = 0, sum = 0;  i < clientDataFormated.size(); i++) {
-			sum = sum + clientDataFormated.get(i);
-			if(i == clientDataFormated.size() - 1)
-				average = sum / i;
+	public static void main(String[] args) throws IOException {
+		
+		Socket clientSocket = new DataServer().startServer();
+		
+		InputStream din = new ObjectInputStream(clientSocket.getInputStream());
+		
+		ObjectMapper objectMapper = JsonMapper.builder().disable(StreamWriteFeature.AUTO_CLOSE_TARGET)
+														.disable(StreamReadFeature.AUTO_CLOSE_SOURCE)
+														.enable(StreamWriteFeature.FLUSH_PASSED_TO_STREAM)
+														.build();
+		
+		DataClient dataClient = objectMapper.readValue(din, DataClient.class);
+		System.out.println("Server has read transfered data: " + dataClient.toString());
+		
+		Integer sum = 0;
+		Float average = null;
+		for(var i : dataClient.getData()) {
+			if((dataClient.getOption().equals("positive") && i < 0) || (dataClient.getOption().equals("negative") && i > 0))
+				i *= -1;
+			sum = sum + i;
 		}
-		ObjectOutputStream dout = new ObjectOutputStream(clientSocket.getOutputStream());
-		dout.writeUTF(average.toString());
-		dout.flush();
-	}
+		average = (float)sum / dataClient.getData().size();
+		
+		OutputStream dout = new ObjectOutputStream(clientSocket.getOutputStream());
+		objectMapper.writeValue(dout, average.toString());
+		System.out.println("Server has been returned data: " + average);
+				
+		DataClient dataClient_1 = objectMapper.readValue(din, DataClient.class);
+		System.out.println("Server has read transfered data: " + dataClient_1.toString());
+		
+		Integer sum_1 = 0;
+		Float average_1 = null;
+		for(var i : dataClient_1.getData()) {
+			if((dataClient_1.getOption().equals("positive") && i < 0) || (dataClient_1.getOption().equals("negative") && i > 0))
+				i *= -1;
+			sum_1 = sum_1 + i;
+		}	
+		average_1 = (float)sum_1 / dataClient_1.getData().size();
+		
+		objectMapper.writeValue(dout, average_1.toString());
+		System.out.println("Server has been returned data: " + average_1);		
+	}		
 }
+
